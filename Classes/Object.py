5d97e71d49a10f1fda9a8b7c9973a5d8e7130256
Joana <mfjotaa@hotmail.com>
2019-02-20 19:50:32 +0100

    Corrections to player

--- Classes/Object.py
+++ Classes/Object.py
@@ -1,48 +1,41 @@
 import pygame
 import random
 from constantes import *
-  #"""
-    #This class represents the ball.
-    #It derives from the "Sprite" class in Pygame.
-    #"""
+
+
+    #This class represents the objects that player have to collect.
 class Object(pygame.sprite.Sprite):
 
+    # Constructor.
+    def __init__(self, image_file,x_display,y_display):
+        # Call the parent class (Sprite) constructor
+        super().__init__()
+        
+        # Create an image of the object.
+        self.image = pygame.image.load(image_file).convert_alpha()
+        #cordinates of object
+        self.x = 0
+        self.y = 0
+        #coordinates to display object in wall
+        self.x_display = x_display
+        self.y_display = y_display
+        # Update the position of this object by setting the values of rect.x and rect.y
+        self.rect = self.image.get_rect()
 
-	def __init__(self, image_file,x_display,y_display):
-		# Constructor. Pass in the color of the block and its size. """
-		# Call the parent class (Sprite) constructor
-		super().__init__()
-		
-		# Create an image of the block.
-		# This could also be an image loaded from the disk.
-		self.image = pygame.image.load(image_file).convert_alpha()
-		#self.image_file = image_file
-		self.x = 0
-		self.y = 0
-		self.x_display = x_display
-		self.y_display = y_display
-		# Fetch the rectangle object that has the dimensions of the image
-		# image.
-		# Update the position of this object by setting the values
-		# of rect.x and rect.y
-		self.rect = self.image.get_rect()
+    #Set of random objects
+    def set_item(self, player, list, level):
+        self.player = (0,0)
+        wlist = []
+        wlist = list
+        x = random.randrange(1,15)
+        y = random.randrange(1,15)
+        #Obstacles verification to random objects
+        for object in wlist:
+            while  object.x == x or object.y == y or level.structure[y][x] == 'm' or level.structure[y][x] == 'a' or level.structure[y][x] == 'd':
+                #random in screen
+                x = random.randrange(1,15)
+                y = random.randrange(1,15)
+        #Position of random objects
+        self.x = x * sprite_size
+        self.y = y * sprite_size
 
-        
-	def set_item(self, player, list, level):
-		self.player = (0,0)
-		wlist = []
-		wlist = list
-		x = random.randrange(1,15)
-		y = random.randrange(1,15)
-		
-		for i in wlist:
-			while  i.x==x or i.y == y or level.structure[y][x] == 'm' or level.structure[y][x] == 'a' or level.structure[y][x] == 'd':
-				x = random.randrange(1,15)
-				y = random.randrange(1,15)
-		
-		self.x = x * sprite_size
-		self.y = y * sprite_size
-		
-		#self.image_file = pygame.image.load(self.image_file).convert_alpha()
-        
-        
